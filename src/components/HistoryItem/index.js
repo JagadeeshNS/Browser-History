7793bf51d2c79a12e037bc049172f8d7f7d5f59b
JagadeeshNS <129928667+JagadeeshNS@users.noreply.github.com>
2023-05-15 15:04:47 +0530

    Update index.js

--- src/components/HistoryItem/index.js
+++ src/components/HistoryItem/index.js
@@ -15,17 +15,20 @@ const HistoryItem = props => {
           <img src={logoUrl} alt="domain logo" className="image" />
           <p className="title">{title}</p>
           <p className="domain">{domainUrl}</p>
-          <div>
-            <button type="button" className="button" onClick={onClickDelete}>
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/delete-ing.png"
-                alt="delete"
-                className="delete"
-              />
-            </button>
-          </div>
         </div>
       </div>
+      <button
+        type="button"
+        onClick={onClickDelete}
+        className="button"
+        data-testid="delete"
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png "
+          alt="delete"
+          className="delete"
+        />
+      </button>
     </li>
   )
 }
